28eae848d00a6510191a4ad922e30f06d7640a70
RitoxicGame <bigmagicrat@gmail.com>
2024-03-23 19:00:44 -0400

    What I have so far
    I am very tired

--- space-trader/Assets/Scripts/Trade/TradeWindow.cs
+++ space-trader/Assets/Scripts/Trade/TradeWindow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SelectionWindow : MonoBehaviour
+public class TradeWindow: MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
