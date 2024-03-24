004ef374ab75e0262fa9efb25f0e26cf3ea93e76
RitoxicGame <bigmagicrat@gmail.com>
2024-03-24 11:53:22 -0400

    Revert "What I have so far"
    This reverts commit 28eae848d00a6510191a4ad922e30f06d7640a70.

--- space-trader/Assets/Scripts/UI/SelectionWindow.cs
+++ space-trader/Assets/Scripts/UI/SelectionWindow.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SelectionWindow : MonoBehaviour
+//Implementation *heavily* inspired by that which was used in Apply to the Industry Simulator, coded chiefly by Rob Reddick and Eduardo Escudero
+
+//public enum WindowType { trade, agent }
+
+public interface ISelectionWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadData();
+    public void DisplayPanels(GridLayoutGroup layoutContainer, GameObject panelPrefab);
+}
+
+public class BuyTradeWindow : ISelectionWindow
+{
+    PlanetMarketInstance planetMarket;
+
+    public void LoadData()
     {
-        
+        planetMarket = TradeManager.Instance.travelManager.currentPlanet.GetComponent<PlanetMarketInstance>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DisplayPanels(GridLayoutGroup layoutContainer, GameObject panelPrefab)
     {
-        
+        foreach (PlanetMarketPosting p in planetMarket.market)
+        {
+            //Create the button
+            GameObject panelButton = Object.Instantiate(panelPrefab);
+
+            //Set it to be a child of the grid layout
+            panelButton.transform.SetParent(layoutContainer.transform);
+
+            //Add the Trade Item data
+            TradeWindowPanel panel = panelButton.GetComponent<TradeWindowPanel>();
+            panel.tradeType = TradeType.playerBuy;
+            panel.item = p.item;
+            panel.Value = planetMarket.GetSalePrice(p.item);
+            panel.stock = p.stockAvailable;
+            panel.ResetUI();
+        }
     }
 }
+
+//Agent display window goes here
